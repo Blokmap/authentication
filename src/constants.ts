e3339778056a165499d5e11ee4505f55bb1d8002
Tibo Ulens <ulens.tibo@gmail.com>
2025-09-03 20:49:06 +0200

    yeetwout

---
+++ src/constants.ts
@@ -0,0 +1,19 @@
+import fs from "node:fs";
+
+const constants = {
+	google_client_id: "",
+	google_client_secret: "",
+};
+
+try {
+	const google_oidc_credentials = fs.readFileSync("/run/secrets/google-oidc-credentials", {encoding: "utf-8"});
+	const google_oidc_parts = google_oidc_credentials.split('\n');
+
+	constants.google_client_id = google_oidc_parts[0] as string;
+	constants.google_client_secret = google_oidc_parts[1] as string;
+} catch (err) {
+	console.error(err);
+	process.exit(1);
+}
+
+export default constants;
--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,11 +1,30 @@
 import express from "express";
 import passport from "passport";
-import GoogleStrategy from "passport-google-oidc";
+import OpenIDConnectStrategy, {
+	type Profile,
+	type VerifyCallback,
+} from "passport-openidconnect";
+
+import constants from "../constants.js";
 
 const router = express.Router();
 
 passport.use(
-	new GoogleStrategy({ clientID: "", clientSecret: "", callbackURL: "", scope: ["profile"] }),
+	new OpenIDConnectStrategy(
+		{
+			issuer: "https://accounts.google.com",
+			authorizationURL: "https://accounts.google.com/o/oauth2/v2/auth",
+			tokenURL: " https://oauth2.googleapis.com/token",
+			userInfoURL: "https://openidconnect.googleapis.com/v1/userinfo",
+			clientID: constants.google_client_id,
+			clientSecret: constants.google_client_secret,
+			callbackURL: "",
+			scope: ["profile"],
+		},
+		(_issuer: string, profile: Profile, cb: VerifyCallback) => {
+			return cb(null, profile);
+		},
+	),
 );
 
 router.get("/sso/google", passport.authenticate("google"));
